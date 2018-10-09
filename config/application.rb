c165adab9ff5b66da2c898d59a2fe8f9f60d6901
Durev <vincent.durewski@gmail.com>
2018-10-10 00:14:23 +0200

    Use SQL db representation instead of ruby representation
    A ruby db dump (schema.rb) does not support enum colons in migrations

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module WodStory
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Use a SQL db representation
+    config.active_record.schema_format = :sql
   end
 end
